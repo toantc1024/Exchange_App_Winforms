9d0f58967c52b605cc6f787b8fabf6d84146e4b3
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-05-07 07:51:24 +0700

    Update: Config carousel

---
+++ ViewModel/DashboardViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Exchange_App.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange_App.ViewModel
+{
+    internal class DashboardViewModel : BaseViewModel
+    {
+        private User _currentUser;
+        public User CurrentUser
+        {
+            get { return _currentUser; }
+            set { _currentUser = value; OnPropertyChanged(nameof(CurrentUser)); }
+        }
+
+        private ProductManagerViewModel _productManagerContent;
+
+        public ProductManagerViewModel ProductManagerContent
+        {
+            get { return _productManagerContent; }
+            set { _productManagerContent = value; OnPropertyChanged(nameof(ProductManagerContent)); }
+        }
+        public DashboardViewModel(User currentUser)
+        {
+
+            CurrentUser=currentUser;
+            ProductManagerContent = new ProductManagerViewModel(CurrentUser);
+        }
+    }
+}
